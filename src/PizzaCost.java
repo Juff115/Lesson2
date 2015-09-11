65b3e1d580e78c79d9a3c7207e24685d177d46e9
jame5380 <jame5380@ISS147D8-24.scdsb.pri>
2015-09-11 10:38:08 -0400

    Finished Pizza Cost

--- src/PizzaCost.java
+++ src/PizzaCost.java
@@ -1,9 +1,21 @@
+import hsa.*;
 
 public class PizzaCost {
 
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        Console c = new Console();
+        double size, topnum, base, top, basein, total;
+        base = 2.25;
+        top = 0.75;
+        basein = 0.5;
+        total= 0;
+        c.print("What size of pizza do you want in inches?: ");
+        size = c.readDouble();
+        c.print("How many toppings do you want?: ");
+        topnum = c.readDouble();
+        total= base + (top * topnum) + (size * basein);
+        c.print("Your total cost for the pizza is: $" + total);
     }
     
 }

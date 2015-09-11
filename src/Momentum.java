9fa2a7b6840232410ab4411aa4dfa7479315e219
jame5380 <jame5380@ISS147D8-24.scdsb.pri>
2015-09-11 10:24:54 -0400

    Finished momentum

--- src/Momentum.java
+++ src/Momentum.java
@@ -1,8 +1,19 @@
+import hsa.*;
 
 public class Momentum {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Console c = new Console();
+        double mass, velo, mome;
+        c.print("Enter object's mass: ");
+        mass = c.readDouble();
+        c.print("Enter object's velocity: ");
+        velo = c.readDouble();
+        //calculate
+        mome = mass * velo;
+        //show results
+        c.print("The momentum is " + mome);
+        
     }
     
 }

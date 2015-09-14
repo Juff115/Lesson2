ada3872cbdac8f94f28cd15a6bb088fecb1e89b7
jame5380 <jame5380@ISS147D8-24.scdsb.pri>
2015-09-14 09:40:37 -0400

    Mak line finished

--- src/MakeLine.java
+++ src/MakeLine.java
@@ -1,9 +1,19 @@
+import TurtleGraphics.*;
 
 public class MakeLine {
 
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        SketchPadWindow s = new SketchPadWindow(400,400);
+        //make pen
+        Pen p = new RainbowPen(s);
+        p.up();
+        //sub 200 for actual
+        p.move(100-200,50-200);
+        p.down();
+        p.drawString("(" + 100 +", "+ 50 + ")");
+        p.move(300-200, 300-200);
+        p.drawString("(" + 300 +", "+ 300 + ")");
     }
     
 }

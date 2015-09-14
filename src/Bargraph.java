3b6f570992ea4a137669b662d6a69b2236d0b4c0
jame5380 <jame5380@ISS147D8-24.scdsb.pri>
2015-09-14 10:02:34 -0400

    Bargraph finished

--- src/Bargraph.java
+++ src/Bargraph.java
@@ -1,9 +1,41 @@
-
+import TurtleGraphics.*;
+import hsa.*;
 public class Bargraph {
 
    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Console c = new Console();
+        SketchPadWindow s = new SketchPadWindow(300,250);
+        //make pen
+        Pen p = new RainbowPen(s);
+        int n1, n2, n3, n4;
+        //get numbers
+        c.print("Enter first number: ");
+        n1 = c.readInt();
+        c.print("Enter second number: ");
+        n2 = c.readInt();
+        c.print("Enter third number: ");
+        n3 = c.readInt();
+        c.print("Enter fourth number: ");
+        n4 = c.readInt();
+        p.setWidth(50);
+        p.up();
+        p.move(30-150, 0-125);
+        p.down();
+        p.move(30-150, n1-125);
+        p.up();
+        p.move(100-150, 0-125);
+        p.down();
+        p.move(100-150, n2-125);
+        p.up();
+        p.move(170-150, 0-125);
+        p.down();
+        p.move(170-150, n3-125);
+        p.up();
+        p.move(240-150, 0-125);
+        p.down();
+        p.move(240-150, n4-125);
+        
     }
     
 }
